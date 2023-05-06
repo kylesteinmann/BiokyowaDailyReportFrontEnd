ba5f8a0a4cb8bd0de31166dec67096a8ca2dd6a5
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2023-05-06 15:29:01 -0500

    added fermentation page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExtractionComponent } from './extraction/extraction.component';
+import { ExtractionComponent } from './Components/extraction/extraction.component';
+import { AppComponent } from './app.component';
+import { FermentationComponent } from 'src/app/Components/fermentation/fermentation.component';
 
 const routes: Routes = [
   { path: 'extraction', component: ExtractionComponent },
-  { path: '', component: ExtractionComponent }
+  { path: 'fermentation', component: FermentationComponent },
+
 ];
 
 @NgModule({
