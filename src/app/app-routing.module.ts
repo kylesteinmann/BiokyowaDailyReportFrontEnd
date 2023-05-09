681770109c5e012e5f5b5e008490b111b34dad20
jonbeal93 <jonbeal93@gmail.com>
2023-05-08 20:17:52 -0500

    auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExtractionComponent } from './extraction/extraction.component';
+import { ExtractionComponent } from './Components/extraction/extraction.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { FermentationComponent } from 'src/app/Components/fermentation/fermentation.component';
 import { SummaryComponent } from './Components/summary/summary.component';
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  signupForm = new FormGroup({
+    email: new FormControl(''),
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    password: new FormControl(''),
+    password_confirmation: new FormControl('')
+  })
+
+  constructor(private http:HttpClient){}
+
+  onSubmit(){
+   const blog = this.signupForm.value;
+
+    this.http.post("http://localhost:3000/api/v1/users/create", blog).subscribe(
+      (res:any)=>(console.log(res))
+    )
+  }
 }
